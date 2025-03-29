d7434e9e346d972feb9f9f2b40bbdb102042df98
Tareq Alsadaqa <tareq.alsadaqa1@gmail.com>
2025-03-30 00:19:59 +0300

    1st commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Infinity_Solutions.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
