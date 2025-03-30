0cb62c4509f8ff844d1e78081090254db778e0ad
Tareq Alsadaqa <tareq.alsadaqa1@gmail.com>
2025-03-30 06:41:41 +0300

    add navbar and footer

--- obj/Debug/net8.0/Infinity-Solutions.AssemblyInfo.cs
+++ obj/Debug/net8.0/Infinity-Solutions.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infinity-Solutions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc6e4a4d6242decf3f23121e6422092a3710be59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7434e9e346d972feb9f9f2b40bbdb102042df98")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infinity-Solutions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infinity-Solutions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
