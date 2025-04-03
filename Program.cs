f74979be2b486ae0a5f5403b23907015f69f414b
Tareq Alsadaqa <tareq.alsadaqa1@gmail.com>
2025-04-03 20:41:39 +0300

    Edit Flutter Page

--- Program.cs
+++ Program.cs
@@ -22,10 +22,8 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-<<<<<<< HEAD
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-=======
+
     pattern: "{controller=Home}/{action=Flutter_page}/{id?}");
->>>>>>> 8e25dfc0f57636dc1bc6b1c2d96415cb1f1da83f
+
 
 app.Run();
